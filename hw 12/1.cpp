e8537c2556a4755192065a7ad74f3926c65ac91c
ashaga45 <ashaga45@gmail.com>
2022-04-08 13:40:16 +0530

    almost over

--- hw 12/1.cpp
+++ hw 12/1.cpp
@@ -1,6 +1,9 @@
 //This program is written by ak9650, Ashwin K Raghu
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
+
 const char DASH = '-',DOLLAR_SYMBOL='$',DECIMAL_SYMBOL='.';
 const int A_DOLLAR_IN_CENTS = 100,ZERO=0,TEN=10;
 
@@ -52,11 +55,14 @@ class Money{
         //connected to a file.
     
         friend ostream& operator<<(ostream& outs, const Money& amount);
-
         //Overloads the << operator so it can be used to output values of type
         //Money. Precedes each output value of type Money with a dollar sign.
         //Precondition: If outs is a file output stream, then outs has already been
         //connected to a file.
+
+        Money(const Money& amount){
+            all_cents = amount.all_cents;
+        }
 };
 
 Money operator+(const Money& amount1, const Money& amount2){
@@ -89,7 +95,7 @@ bool operator<(const Money& amount1, const int &val){
     return amount1.all_cents < val;
 }
 
-istream& operator>>(istream& ins, Money& amount){
+istream& operator>>(std::istream& ins, Money& amount){
     char first, decimal;
     bool negative;
     long dollars;
@@ -135,65 +141,128 @@ ostream& operator<<(ostream& outs, const Money& amount){
     return outs;
 }
 
-class Cheque{
+class check{
     private:
-        int cheque_number;
-        Money cheque_amount;
-        bool cheque_cashed;
+        int check_number;
+        Money check_amount;
+        bool check_cashed;
     public:
-        int get_cheque_number() const{
-            return cheque_number;
+        int get_check_number() const{
+            return check_number;
         }
-        Money get_cheque_amount() const{
-            return cheque_amount;
+        Money get_check_amount() const{
+            return check_amount;
         }
-        bool get_cheque_cashed() const{
-            return cheque_cashed;
-        }
-
-        void set_cheque_number(const int cheque_num){
-            cheque_number = cheque_num;
+        bool get_check_cashed() const{
+            return check_cashed;
         }
 
-        void set_cheque_amount(const Money &amt){
-            cheque_amount = amt;
+        void set_check_number(const int check_num){
+            check_number = check_num;
         }
 
-        void set_cheque_cashed(const bool cashed){
-            cheque_cashed = cashed;
+        void set_check_amount(const Money &amt){
+            check_amount = amt;
         }
 
-        friend istream& operator>>(istream& ins, Cheque& cheque);
-        friend ostream& operator<<(ostream& outs, Cheque& cheque);
-
-        bool operator<(const Cheque& chk){
-            return cheque_number<chk.cheque_number;
+        void set_check_cashed(const bool cashed){
+            check_cashed = cashed;
         }
-}
+
+        friend istream& operator>>(istream& ins, check& check);
+        friend ostream& operator<<(ostream& outs, check& check);
+
+        bool operator<(const check& chk){
+            return check_number<chk.check_number;
+        }
+};
 
 
-istream& operator>>(istream& ins, Cheque& cheque){
+istream& operator>>(istream& ins, check& check){
     char cashed;
-    ins>>cheque.cheque_number>>cheque.cheque_amount>>cashed_status;
+    ins>>check.check_number>>check.check_amount>>cashed;
     if(cashed=='y' || cashed =='Y'){
-        cheque.cheque_cashed = true;
+        check.check_cashed = true;
     }
     else if(cashed=='n' || cashed =='N'){
-        cheque.cheque_cashed = false;
+        check.check_cashed = false;
     }
     else{
-        cout<<"Error in inputs of cheque";
+        cout<<"Error in inputs of check";
     }
 
     return ins;
 
 }
-ostream& operator<<(ostream& outs, Cheque& cheque){
-    outs<<cheque.cheque_number<<cheque.cheque_amount;
-    if(cheque.cheque_cashed){
-        cout<<"Cheque cashed."
+ostream& operator<<(ostream& outs, check& check){
+    outs<<check.check_number<<check.check_amount;
+    if(check.check_cashed){
+        cout<<"check cashed.";
     }
     else{
-        cout<<"Cheque not cashed."
+        cout<<"check not cashed.";
     }
+    return outs;
+}
+
+int main(){
+    vector<check> checks;
+    Money new_balance, curr_balance, old_balance,total_deposit, curr_deposit, deposit_input, checks_cashed_amount;
+    check check_input;
+    char flag;
+
+    cout<<"Hello."<<endl;
+    cout<<"Enter your current bank balance (format: $dollars.cents or -$dollars.cents where cents < "<<A_DOLLAR_IN_CENTS<<")";
+    cin>>curr_balance;
+    cout<<"Enter your old bank balance (format: $dollars.cents or -$dollars.cents where cents < "<<A_DOLLAR_IN_CENTS<<")";
+    cin>>old_balance;
+    cout<<"Would you like to add a deposit? (y/n)";
+    cin>>flag;
+    while(flag=='y' || flag=='Y'){
+        cout<<"Enter deposit amount (format: $dollars.cents where cents < "<<A_DOLLAR_IN_CENTS<<")";
+        cin>>curr_deposit;
+        total_deposit = total_deposit + curr_deposit;
+
+        cout<<"Would you like to add another deposit? (y/n)";
+        cin>>flag;
+    }
+
+    cout<<"Would you like to add check information? (y/n)";
+    cin>>flag;
+
+    while(flag=='y' || flag=='Y'){
+        cout<<"Enter check Id, check amount, y/n indicating the check is cashed or not. (egs: 12445 $10.34 y)";
+        cin>>check_input;
+        checks.push_back(check_input);
+        if(check_input.get_check_cashed()){
+            checks_cashed_amount = checks_cashed_amount + check_input.get_check_amount();
+        }
+        cout<<"Would you like to add another check information? (y/n)";
+        cin>>flag;
+    }
+    
+    new_balance = old_balance + total_deposit - checks_cashed_amount;
+
+    cout<<"Total of checks cashed: "<<checks_cashed_amount;
+    cout<<"Total of deposits: "<<total_deposit;
+    cout<<"New balance calculated: "<<new_balance;
+    cout<<"Difference of new balance calculated from current balance"<<new_balance - curr_balance;
+
+    sort(checks.begin(),checks.end());
+    
+    cout<<"Checks cashed previous balancing:";
+    for(int i=0;i<checks.size();i++){
+        if(checks[i].get_check_cashed()){
+            cout<<checks[i]<<endl;
+        }
+    }
+
+    cout<<"Checks not cashed:";
+    for(int i=0;i<checks.size();i++){
+        if(!checks[i].get_check_cashed()){
+            cout<<checks[i]<<endl;
+        }
+    }
+
+    return 0;
 }
