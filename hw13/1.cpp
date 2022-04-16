d750a270f5d9a05c1adf5bae6b165a7dbeadefaf
ashaga45 <ashaga45@gmail.com>
2022-04-16 22:46:26 +0530

    almost done

--- hw13/1.cpp
+++ hw13/1.cpp
@@ -16,21 +16,27 @@ class Organism{
         Organism(int critter_type) : critter(critter_type), moved_recently(false), breeding(0){}
         virtual void move() = 0;
         virtual void resetStarving(){};
+        virtual int getStarving() const{
+            return 0;
+        }
         
         int get_critter() const{
             return critter;
         }
 
-        bool recently_moved(){
+        bool recently_moved() const{
             return moved_recently;
         }
 
-        int getBreeding(){
+        int getBreeding() const{
             return breeding;
         }
 
         void resetBreeding(){
             breeding = 0;
+        }
+        void resetMove(){
+            moved_recently = false;
         }
 };
 
@@ -56,6 +62,10 @@ class Doodlebug : public Organism{
         }
         void resetStarving(){
             starving = 0;
+        }
+
+        int getStarving() const{
+            return starving;
         }
         
 };
@@ -91,155 +101,20 @@ class Grid{
             return inbound;
         }
 
-        void moveDoodleBugs(){
-            int shift_x, shift_y;
+        void starve(){
             for(int x=0;x<width;x++)
                 for(int y=0;y<height;y++){
-                    if(cells[x][y]==nullptr){
-                        continue;
-                    }
-                    else if((!(cells[x][y]->recently_moved())) && (cells[x][y]->get_critter()==critter_DOODLEBUG)){
-
-                        int neighbors_x[] =  {x-1,x+1,x,x}, neighbors_y[] =  {y,y,y+1,y-1};
-                        bool ate_ant = false;
-                        for(int k=0;k<LEN_NEIGHBOR;k++){
-                            if(inbounds(neighbors_x[k],neighbors_y[k])){
-                                if((!ate_ant)&&(cells[neighbors_x[k]][neighbors_x[y]])&&(cells[neighbors_x[k]][neighbors_x[y]]->get_critter()==critter_ANT)){
-                                    delete cells[neighbors_x[k]][neighbors_y[k]];
-                                    cells[neighbors_x[k]][neighbors_y[k]] = cells[x][y];
-                                    cells[x][y] = nullptr;
-                                    cells[neighbors_x[k]][neighbors_y[k]]->move();
-                                    cells[neighbors_x[k]][neighbors_y[k]]->resetStarving();
-                                    ate_ant = true;
-                                }
-                                else{
-                                    continue;
-                                }
-
-                            }
-                            else{
-                                continue;
-                            }
-
-                        }
-                        if(!ate_ant){
-                            int neighbor = rand()%4;
-                            if(neighbor==LEFT){
-                                shift_x = -1;
-                                shift_y = 0;
-                            }
-                            else if(neighbor==RIGHT){
-                                shift_x = 1;
-                                shift_y = 0;
-                            }
-                            else if(neighbor==UP){
-                                shift_x = 0;
-                                shift_y = 1;
-                            }
-                            else if(neighbor==DOWN){
-                                shift_x = 0;
-                                shift_y = -1;
-                            }
-                            else{
-                                shift_x = 0;
-                                shift_y = 0;
-                            }
-
-                            int x_new = x + shift_x, y_new = x+shift_y;
-                            if(inbounds(x_new,y_new)&&(!cells[x_new][y_new])){
-                                cells[x_new][y_new] = cells[x][y];
-                                cells[x_new][y_new]->move();
-                                cells[x][y] = nullptr;
-                            }
-                        }
-                    }
-                    else{
-                        continue;
-                    }
-                    
-
-                }
-
-            
-        }
-
-        void moveAnts(){
-            for(int x=0;x<width;x++)
-                for(int y=0;y<height;y++){
-                    if(cells[x][y]==nullptr){
-                        continue;
-                    }
-                    else if((!(cells[x][y]->recently_moved())) && (cells[x][y]->get_critter()==critter_ANT)){
-                        int neighbor = rand()%4,shift_x,shift_y;
-                        if(neighbor==LEFT){
-                            shift_x = -1;
-                            shift_y = 0;
-                        }
-                        else if(neighbor==RIGHT){
-                            shift_x = 1;
-                            shift_y = 0;
-                        }
-                        else if(neighbor==UP){
-                            shift_x = 0;
-                            shift_y = 1;
-                        }
-                        else if(neighbor==DOWN){
-                            shift_x = 0;
-                            shift_y = -1;
-                        }
-                        else{
-                            shift_x = 0;
-                            shift_y = 0;
-                        }
-
-                        int x_new = x + shift_x, y_new = x+shift_y;
-                        if(inbounds(x_new,y_new)&&(!cells[x_new][y_new])){
-                            cells[x_new][y_new] = cells[x][y];
-                            cells[x_new][y_new]->move();
-                            cells[x][y] = nullptr;
-                        }
-                    }
-                    else{
-                        continue;
+                    if((cells[x][y]) && (cells[x][y]->get_critter()==critter_DOODLEBUG) && (cells[x][y]->getStarving()>=doodlebug_starving)){
+                        delete cells[x][y];
+                        cells[x][y] = nullptr;
                     }
                 }
         }
+        void moveDoodleBugs();
+        void moveAnts();
+        void breed();
 
-        void breed(){
-            for(int x=0;x<width;x++)
-                for(int y=0;y<height;y++){
-                    if(cells[x][y]==nullptr){
-                        continue;
-                    }
-                    else{
-                        int breed_flag = false;
-                        if((cells[x][y]->get_critter()==critter_ANT) && (cells[x][y]->getBreeding()>=ant_breed_steps)){
-                            int neighbors_x[] =  {x-1,x+1,x,x}, neighbors_y[] =  {y,y,y+1,y-1};
-                            for(int k=0;k<LEN_NEIGHBOR;k++){
-                                if(inbounds(neighbors_x[k],neighbors_y[k]) && (!breed_flag) &&(!cells[neighbors_x[k]][neighbors_y[k]])){
-                                    cells[neighbors_x[k]][neighbors_y[k]] = new Ant();
-                                    cells[x][y]->resetBreeding();
-                                    breed_flag = true;
-                                }
-                                
-                            }
-                        }
-                        else if((cells[x][y]->get_critter()==critter_DOODLEBUG) && (cells[x][y]->getBreeding()>=doodlebug_breed_steps)){
-                            int neighbors_x[] =  {x-1,x+1,x,x}, neighbors_y[] =  {y,y,y+1,y-1};
-                            for(int k=0;k<LEN_NEIGHBOR;k++){
-                                if(inbounds(neighbors_x[k],neighbors_y[k]) && (!breed_flag) &&(!cells[neighbors_x[k]][neighbors_y[k]])){
-                                    cells[neighbors_x[k]][neighbors_y[k]] = new Doodlebug();
-                                    cells[x][y]->resetBreeding();
-                                    breed_flag = true;
-                                }
-                                
-                            }
-                        }
 
-                        }
-                    }
-
-        }
 
     public:
         Grid(int inp_height, int inp_width, int inp_num_ants, int inp_num_doodlebugs, int inp_doodlebug_breed_steps, int inp_ant_breed_steps, int inp_doodlebug_starving) : timestep(0), height(inp_height),width(inp_width){
@@ -258,14 +133,14 @@ class Grid{
 
         void showGrid(){
             cout<<"Timestep: "<<timestep<<endl;
-            for(int i=0;i<width;i++){
-                for(int j=0;j<height;j++){
-                    if(cells[i][j]){
-                        if(cells[i][j]->get_critter()==critter_ANT){
+            for(int x=0;x<width;x++){
+                for(int y=0;y<height;y++){
+                    if(cells[x][y]){
+                        if(cells[x][y]->get_critter()==critter_ANT){
                             cout<<"o ";
 
                         }
-                        else if(cells[i][j]->get_critter()==critter_DOODLEBUG){
+                        else if(cells[x][y]->get_critter()==critter_DOODLEBUG){
                             cout<<"X ";
 
                         }
@@ -279,13 +154,176 @@ class Grid{
             }
 
         }
+
+        void resetMovements(){
+            for(int x=0;x<width;x++)
+                for(int y=0;y<height;y++){
+                    if(cells[x][y]){
+                        cells[x][y]->resetMove();
+                    }
+                }
+            
+        }
         void nextTimeStep(){
             timestep++;
             moveDoodleBugs();
             moveAnts();
+            breed();
+            starve();
             showGrid();
+            resetMovements();
         }
 };
+
+void Grid::moveAnts(){
+    for(int x=0;x<width;x++)
+        for(int y=0;y<height;y++){
+            if(cells[x][y]==nullptr){
+                continue;
+            }
+            else if((!(cells[x][y]->recently_moved())) && (cells[x][y]->get_critter()==critter_ANT)){
+                int neighbor = rand()%4,shift_x,shift_y;
+                if(neighbor==LEFT){
+                    shift_x = -1;
+                    shift_y = 0;
+                }
+                else if(neighbor==RIGHT){
+                    shift_x = 1;
+                    shift_y = 0;
+                }
+                else if(neighbor==UP){
+                    shift_x = 0;
+                    shift_y = 1;
+                }
+                else if(neighbor==DOWN){
+                    shift_x = 0;
+                    shift_y = -1;
+                }
+                else{
+                    shift_x = 0;
+                    shift_y = 0;
+                }
+
+                int x_new = x + shift_x, y_new = x+shift_y;
+                if(inbounds(x_new,y_new)&&(!cells[x_new][y_new])){
+                    cells[x_new][y_new] = cells[x][y];
+                    cells[x_new][y_new]->move();
+                    cells[x][y] = nullptr;
+                }
+            }
+            else{
+                continue;
+            }
+        }
+}
+
+void Grid::breed(){
+    for(int x=0;x<width;x++)
+        for(int y=0;y<height;y++){
+            if(cells[x][y]==nullptr){
+                continue;
+            }
+            else{
+                int breed_flag = false;
+                if((cells[x][y]->get_critter()==critter_ANT) && (cells[x][y]->getBreeding()>=ant_breed_steps)){
+                    int neighbors_x[] =  {x-1,x+1,x,x}, neighbors_y[] =  {y,y,y+1,y-1};
+                    for(int k=0;k<LEN_NEIGHBOR;k++){
+                        if(inbounds(neighbors_x[k],neighbors_y[k]) && (!breed_flag) &&(!cells[neighbors_x[k]][neighbors_y[k]])){
+                            cells[neighbors_x[k]][neighbors_y[k]] = new Ant();
+                            cells[x][y]->resetBreeding();
+                            breed_flag = true;
+                        }
+                        
+                    }
+                }
+                else if((cells[x][y]->get_critter()==critter_DOODLEBUG) && (cells[x][y]->getBreeding()>=doodlebug_breed_steps)){
+                    int neighbors_x[] =  {x-1,x+1,x,x}, neighbors_y[] =  {y,y,y+1,y-1};
+                    for(int k=0;k<LEN_NEIGHBOR;k++){
+                        if(inbounds(neighbors_x[k],neighbors_y[k]) && (!breed_flag) &&(!cells[neighbors_x[k]][neighbors_y[k]])){
+                            cells[neighbors_x[k]][neighbors_y[k]] = new Doodlebug();
+                            cells[x][y]->resetBreeding();
+                            breed_flag = true;
+                        }
+                        
+                    }
+                }
+
+                }
+            }
+
+}
+
+void Grid::moveDoodleBugs(){
+    int shift_x, shift_y;
+    for(int x=0;x<width;x++)
+        for(int y=0;y<height;y++){
+            if(cells[x][y]==nullptr){
+                continue;
+            }
+            else if((!(cells[x][y]->recently_moved())) && (cells[x][y]->get_critter()==critter_DOODLEBUG)){
+
+                int neighbors_x[] =  {x-1,x+1,x,x}, neighbors_y[] =  {y,y,y+1,y-1};
+                bool ate_ant = false;
+                for(int k=0;k<LEN_NEIGHBOR;k++){
+                    if(inbounds(neighbors_x[k],neighbors_y[k])){
+                        if((!ate_ant)&&(cells[neighbors_x[k]][neighbors_x[y]])&&(cells[neighbors_x[k]][neighbors_x[y]]->get_critter()==critter_ANT)){
+                            delete cells[neighbors_x[k]][neighbors_y[k]];
+                            cells[neighbors_x[k]][neighbors_y[k]] = cells[x][y];
+                            cells[x][y] = nullptr;
+                            cells[neighbors_x[k]][neighbors_y[k]]->move();
+                            cells[neighbors_x[k]][neighbors_y[k]]->resetStarving();
+                            ate_ant = true;
+                        }
+                        else{
+                            continue;
+                        }
+
+                    }
+                    else{
+                        continue;
+                    }
+
+                }
+                if(!ate_ant){
+                    int neighbor = rand()%4;
+                    if(neighbor==LEFT){
+                        shift_x = -1;
+                        shift_y = 0;
+                    }
+                    else if(neighbor==RIGHT){
+                        shift_x = 1;
+                        shift_y = 0;
+                    }
+                    else if(neighbor==UP){
+                        shift_x = 0;
+                        shift_y = 1;
+                    }
+                    else if(neighbor==DOWN){
+                        shift_x = 0;
+                        shift_y = -1;
+                    }
+                    else{
+                        shift_x = 0;
+                        shift_y = 0;
+                    }
+
+                    int x_new = x + shift_x, y_new = x+shift_y;
+                    if(inbounds(x_new,y_new)&&(!cells[x_new][y_new])){
+                        cells[x_new][y_new] = cells[x][y];
+                        cells[x_new][y_new]->move();
+                        cells[x][y] = nullptr;
+                    }
+                }
+            }
+            else{
+                continue;
+            }
+            
+
+        }
+
+    
+}
 
 int main(){
     srand(time(NULL));
